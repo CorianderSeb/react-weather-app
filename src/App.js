8e5cdc3f4de3912257f4f231dfb7443e9757a6bd
CorianderSeb <106112163+CorianderSeb@users.noreply.github.com>
2022-10-10 19:28:00 -0400

    Added GitHub link and portfolio link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      Weather app
+      <footer>
+        This project was coded by <a href="https://sebastiancseithel.com/" target="_blank"> Sebastian Seithel</a> and {" "} 
+         <a
+          href="https://github.com/CorianderSeb/react-weather-app"
+          target="_blank"
+         >open sourced on GitHub.</a>
+      </footer>
     </div>
   );
 }
